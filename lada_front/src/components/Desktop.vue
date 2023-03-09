c6953c76b6ec0a7898108fec47ea737614f4c944
mateuszpob <mateuszpob@gmail.com>
2023-03-09 18:48:45 +0100

    Update Desktop

--- lada_front/src/components/Desktop.vue
+++ lada_front/src/components/Desktop.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="wrapper d-flex justify-content-around flex-wrap mt-3">
-        <!-- <IndicatorStrap v-for="(value, label) in dashboard" :scalePoints="[0, 50, 100]" :total_items="Object.keys(dashboard).length" :label="label" :value="value" v-bind:key="label"/> -->
-    
+  <div class="wrapper d-flex flex-column align-items-center justify-content-center">
+      <!-- <IndicatorStrap v-for="(value, label) in dashboard" :scalePoints="[0, 50, 100]" :total_items="Object.keys(dashboard).length" :label="label" :value="value" v-bind:key="label"/> -->
+  
+      <div class="d-flex justify-content-around flex-wrap mt-3">
         <IndicatorStrap v-for="(settings, key) in indicators" v-bind:key="key" :scalePoints="settings.scalePoints" :unit="settings.unit" :total_items="1" :label="settings.label" :value="dashboard[key]"></IndicatorStrap>
-    
-        <IndicatorSpeedDigital :unit="'Km/h'" :total_items="1" :label="'SPEED'" :value="120"></IndicatorSpeedDigital>
+      </div>
+      <br>
+      <div>
+        <IndicatorSpeedDigital :unit="'Km/h'" :total_items="1" :label="'SPEED'" :value="Math.round(dashboard['vssSpeed'])"></IndicatorSpeedDigital>
+        <div class="d-flex flex-row justify-content-between">
+          <div class="medium-label digital-font">GEAR: {{ dashboard['gear'] }}</div>
+          <div class="medium-label digital-font">RPM: {{ dashboard['RPM'] }}</div>
+        </div>
+      </div> 
+      
+      <div class="d-flex justify-content-around flex-wrap mt-3">
+        <IndicatorStrap v-for="(settings, key) in indicators2" v-bind:key="key" :scalePoints="settings.scalePoints" :unit="settings.unit" :total_items="1" :label="settings.label" :value="dashboard[key]"></IndicatorStrap>
+      </div>
 
-    </div>
+  </div>
 </template>
 
 <script>
@@ -14,63 +26,72 @@ import IndicatorDigitDefault from './indicators/IndicatorDigitDefault.vue'
 import IndicatorStrap from './indicators/IndicatorStrap.vue' 
 import IndicatorSpeedDigital from './indicators/IndicatorSpeedDigital.vue' 
 export default {
-  name: 'Desktop',
-  data()  {
-    return {
-      dashboard: {},
-      indicators: {
-        "CLT": {label: "WATER TEMP", unit: "°C", scalePoints: [0, 50, 100, 150]},
-        "IAT": {label: "INTAKE AIR TEMP", unit: "°C", scalePoints: [0, 50, 100]},
-        "Batt": {label: "VOLTAGE", unit: "V", scalePoints: [0, 5, 10, 15]},
-        "wboAFR": {label: "AFR", unit: "", scalePoints: [0,  10, 20]}
-      }
+name: 'Desktop',
+data()  {
+  return {
+    dashboard: {},
+    indicators: {
+      "TPS": {label: "TPS", unit: "%", scalePoints: [0, 50, 100]},
+      "IAT": {label: "INTAKE AIR TEMP", unit: "°C", scalePoints: [0, 50, 100]},
+      "Batt": {label: "VOLTAGE", unit: "V", scalePoints: [0, 5, 10, 15]},
+      "wboAFR": {label: "AFR", unit: "", scalePoints: [0,  10, 20]},
+    },
+    indicators2: {
+      "CLT": {label: "WATER TEMP", unit: "°C", scalePoints: [0, 50, 100, 150]},
+      "oilPressure": {label: "OIL PRESSURE", unit: "bar", scalePoints: [0, 2, 4, 6]},
+      "oilTemperature": {label: "OIL TEMP", unit: "°C", scalePoints: [0, 50, 100, 150]},
+      "afrTarget": {label: "TARGET AFR", unit: "", scalePoints: [0, 50, 100]},
+      // "fuelLevel": {label: "FUEL", unit: "%", scalePoints: [0, 50, 100]},
     }
-
-  },
-  props: {
-    msg: String
-  },
-  created() {
-    this.emitter.on("action", action => {
-      if(action === "escape")
-        this.escape();
-    });
-    this.emitter.on("dashframe", frame => {
-      let menu = this.$store.getters.getMenu;
-      
-      Object.keys(frame).forEach((field) => {
-          if(menu[field]) {
-            this.dashboard[field] = frame[field]
-          }
-      })
-    });
-  },
-  mounted() {
-
-  },
-  beforeUnmount() {  
-    this.emitter.off("escape");
-    this.emitter.off("dashframe");
-  },
-  methods: {
-    escape() {
-      switch(true){
-        default:
-          this.$router.push({ name: 'menu' })
-      }
-    }
-  },
-  components: {
-    IndicatorDigitDefault,
-    IndicatorStrap,
-    IndicatorSpeedDigital
   }
+},
+props: {
+  msg: String
+},
+created() {
+  this.emitter.on("action", action => {
+    if(action === "escape")
+      this.escape();
+  });
+  this.emitter.on("dashframe", frame => {
+    let menu = this.$store.getters.getMenu;
+    
+    Object.keys(frame).forEach((field) => {
+        
+          this.dashboard[field] = frame[field]
+        
+    })
+  });
+},
+mounted() {
+},
+beforeUnmount() {  
+  this.emitter.off("escape");
+  this.emitter.off("dashframe");
+},
+methods: {
+  escape() {
+    switch(true){
+      default:
+        this.$router.push({ name: 'menu' })
+    }
+  }
+},
+components: {
+  IndicatorDigitDefault,
+  IndicatorStrap,
+  IndicatorSpeedDigital
+}
 }
 </script>
 
 <style scoped>
 .wrapper {
-  padding: 5px;
-  height: 100%;
+padding: 5px;
+height: 100%;
+}
+.medium-label {
+font-size: 50px;
+line-height: 50px;
 }
 </style>
--- lada_front/src/components/indicators/IndicatorSpeedDigital.vue
+++ lada_front/src/components/indicators/IndicatorSpeedDigital.vue
@@ -1,121 +1,58 @@
 <template>
-    <div class="box">
-      <div class="outer">
-        <div class="inner">
-
-
-          <div class="circle-wrap">
-    <div class="circle">
-      <div class="mask full-1">
-        <div class="fill-1"></div>
-      </div>
-      <div class="mask half">
-        <div class="fill-1"></div>
-      </div>
-      <div class="inside-circle"> 85% </div>
-    </div>
-  </div>
-
-  
-          <div :style="{'font-size':'200px', 'line-height': '200px'}" class="label digital-font">{{ value }}</div>
+  <div class="box">
+    <div class="outer">
+      <div class="inner">
+        <div class="d-flex flex-row align-items-end justify-content-end">
+          <div :style="{'font-size':'200px', 'line-height': '170px'}" class="label digital-font">{{ value }}</div>
+          <div class="unit digital-font">{{ unit }}</div>
         </div>
       </div>
-      
     </div>
+    
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndicatorSpeedDigital',
-  props: {
-    label: String,
-    value: Number,
-    total_items: Number,
-    scalePoints: Array,
-    unit: String
+name: 'IndicatorSpeedDigital',
+props: {
+  label: String,
+  value: Number,
+  total_items: Number,
+  scalePoints: Array,
+  unit: String
+},
+computed: {
+  width() {
+    return (this.value / this.scalePoints[this.scalePoints.length - 1] * 100) + '%';
   },
-  computed: {
-    width() {
-      return (this.value / this.scalePoints[this.scalePoints.length - 1] * 100) + '%';
-    },
-    fontSize() {
-      return 480 / Math.ceil(this.total_items / 3) + 'px'
-    },
-    boxWidth() {
-      return (800 / Math.ceil(this.total_items / 3)) - 4 + 'px'
-    }
+  fontSize() {
+    return 480 / Math.ceil(this.total_items / 3) + 'px'
   },
-  mounted() {
-
-  },
-  methods: {
-    // escape() {
-    //   switch(true){
-    //     default:
-    //       this.$router.push({ name: 'menu' })
-    //   }
-    // }
+  boxWidth() {
+    return (800 / Math.ceil(this.total_items / 3)) - 4 + 'px'
   }
+},
+mounted() {
+},
+methods: {
+  // escape() {
+  //   switch(true){
+  //     default:
+  //       this.$router.push({ name: 'menu' })
+  //   }
+  // }
+}
 }
 </script>
 
 <style scoped>
-.circle-wrap {
-  display: grid;
-  grid-template-columns: repeat(1, 160px);
-  grid-gap: 80px;
-  margin-top: 100px;
-  margin-left: 50px;
-  width: 150px;
-  height: 150px;
-  background: #d9d7da;
-  border-radius: 50%;
+.box {
+height: 170px;
+width:350px;
+text-align: right;
 }
-
-.circle-wrap .circle .mask,
-.circle-wrap .circle .fill-1,
-.circle-wrap .circle .fill-2,
-.circle-wrap .circle .fill-3 {
-  width: 150px;
-  height: 150px;
-  position: absolute;
-  border-radius: 50%;
+.unit {
+font-size: 80px;
 }
-
-.circle-wrap .circle .mask {
-  clip: rect(0px, 150px, 150px, 75px);
-}
-
-.circle-wrap .inside-circle {
-  width: 124px;
-  height: 124px;
-  border-radius: 50%;
-  background: #212020;
-  line-height: 120px;
-  text-align: center;
-  margin-top: 13px;
-  margin-left: 13px;
-  color: white;
-  position: absolute;
-  z-index: 100;
-  font-weight: 700;
-  font-size: 2em;
-}
-
-/* color animation */
-
-.mask .fill-1 {
-  clip: rect(0px, 75px, 150px, 0px);
-  background-color: #ec0c0c;
-}
-
-.mask.full-1,
-.circle .fill-1 {
-  animation: fill-1 ease-in-out 3s;
-  transform: rotate(153deg);
-}
-
-
-
-
 </style>
