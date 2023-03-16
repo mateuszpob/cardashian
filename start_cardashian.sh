0e1e8d28f50c02569f984e0d470df8cb4bf5b13b
mateuszpob <mateuszpob@gmail.com>
2023-03-16 22:39:08 +0100

    fix start script

--- start_cardashian.sh
+++ start_cardashian.sh
@@ -3,8 +3,8 @@
 # kill backend
 ./stop_cardashian.sh
 
-# emuserial will start in core
-# ./emuserialpc/emuserial -r ./emuserialpc/emulogs/emulog_12-03-23_15:29:21 > ./logs/emuserial.log 2>&1 &
+
+./emuserialpc/emuserial -r ./emuserialpc/emulogs/emulog_12-03-23_15:29:21 > ./logs/emuserial.log 2>&1 &
 python3 ./core/main.py -m > ./logs/core.log 2>&1 &
 
 cd ./lada_front
