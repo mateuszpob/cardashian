b0fc18d3a79308ccc81aac8c059778f5eb719458
mateuszpob <mateuszpob@gmail.com>
2023-03-28 10:19:58 +0200

    smos - image build scripts

---
+++ smos/build.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+echo -e '\e[12m[SMOS] - Start creating the os image at: '; date; echo -e '\e[0m';
+
+
+
+
+echo -e '\e[12m[QPS] - Extract, resize partition and mount image\e[0m'
+#IMAGE_NAME=$(unzip -Z1 raspbian_lite_latest)
+
+# ================================ RESIZE FS ========================================================
+IMAGE_NAME=$1
+echo 'Process image: '$IMAGE_NAME
+#unzip -o -q raspbian_lite_latest
+SIZE_PRE=$(ls -l $IMAGE_NAME  | cut -d " " -f5)
+# total = 3909091328; total - 8 = 3900702720
+TOTAL_FLASH_SIZE=3900702720
+TO_ADD=$(($TOTAL_FLASH_SIZE-$SIZE_PRE))
+
+dd if=/dev/zero count=$(($TO_ADD / 512)) >> $IMAGE_NAME
+
+losetup -f -P $IMAGE_NAME
+parted /dev/loop0 resizepart 2 99%
+e2fsck -f /dev/loop0p2
+resize2fs /dev/loop0p2
+PARTUUID=$(blkid -o export /dev/loop0p2 | grep PARTUUID | cut -c 10-100 | rev | cut -c 4- | rev)
+
+echo 'PARTUUID after part '$PARTUUID'\e[0m'
+
+mount -o rw /dev/loop0p2 /mnt
+mount -o rw /dev/loop0p1 /mnt/boot
+echo 'dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=PARTUUID='$PARTUUID'-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait' > /mnt/boot/cmdline.txt
+cp /mnt/etc/fstab fstab_tmp
+
+sed 's/PARTUUID=[0-9a-fA-F]*/PARTUUID='$PARTUUID'/g' fstab_tmp > /mnt/etc/fstab
+
+rm fstab_tmp
+cat /mnt/etc/fstab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo -e '\e[32m[QPS] - Copy install script\e[0m'
+cp ./configure.sh /mnt/root/;
+
+
+mount -o bind /dev /mnt/dev/;
+mount -o bind /proc /mnt/proc/;
+mount -o bind /sys /mnt/sys/;
+mount -o bind /dev/pts /mnt/dev/pts/;
+
+chmod 777 /mnt/tmp;
+cp /etc/resolv.conf /mnt/etc/;
+
+sed -i 's/^/#/g' /mnt/etc/ld.so.preload
+cp /usr/bin/qemu-arm-static /mnt/usr/bin/
+
+echo -e '\e[32m[QPS] - Chroot rootfs\e[0m'
+#chroot /mnt /root/configure.sh
+#
+#sed -i 's/^#//g' /mnt/etc/ld.so.preload
+#
+#umount /mnt/{dev/pts,dev,sys,proc,boot,}
+#
+#losetup -d /dev/loop0;
+#
+#echo -e '\e[32m[QPS] - Finish creating the os image at: '; date; echo -e '\e[0m';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
