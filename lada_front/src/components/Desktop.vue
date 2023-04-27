9d2d3230ccecc30d80dd89cb1c5f522e0c3c05cd
mateuszpob <mateuszpob@gmail.com>
2023-04-27 22:48:06 +0200

    desktop changes

--- lada_front/src/components/Desktop.vue
+++ lada_front/src/components/Desktop.vue
@@ -17,16 +17,18 @@
 
         <div style="" class="d-flex flex-column justify-content-center">
           <div class="d-flex flex-row align-items-end mt-5">
-            <div class="w-label fnt-20" style="width:70px;"></div>
-            <div class="digital-font fnt-100">{{ dashboard['gear'] }}</div>
+            <div class="w-label fnt-20" style="width:70px;">GEAR</div>
+            <div class="digital-font fnt-100" style="width:80px;">{{ dashboard['gear'] }}</div>
           </div>
-          <div class="d-flex flex-row align-items-end mt-5">
+          <div class="d-flex flex-row align-items-end mt-4">
             <div class="w-label fnt-16" style="width:70px;">TOTAL</div>
-            <div class="digital-font fnt-50">{{ dashboard['total_distance'] }}</div>
+            <div class="digital-font fnt-50" style="width:80px;">{{ dashboard['total_distance'] }}</div>
+            <div class="w-label fnt-16" style="margin-left: 10px;">km</div>
           </div>
           <div class="d-flex flex-row align-items-end mt-2">
             <div class="w-label fnt-16" style="width:70px;">TRIP</div>
-            <div class="digital-font fnt-50">{{ dashboard['trip_distance'] }}</div>
+            <div class="digital-font fnt-50" style="width:80px;">{{ dashboard['trip_distance'] }}</div>
+            <div class="w-label fnt-16" style="margin-left: 10px;">km</div>
           </div>
         </div>
 s
