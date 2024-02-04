ff458917e9d7b05f34c19d78a81a4a006090c614
mateuszpob <mateuszpob@gmail.com>
2024-02-04 13:14:15 +0100

    center map

--- displayapp/src/components/Maps.vue
+++ displayapp/src/components/Maps.vue
@@ -2,24 +2,41 @@
   <div class="map-container">
     <GotToMenu :context="dashboard" :x="20" :y="20"></GotToMenu>
     <div id="map" class="map-container"></div>
+        <button @click="centerMap" id="autoUpdateButton">CR</button>
   </div>
 </template>
 
 <script>
 import { initializeMap } from '../logic/maps.js';
+
 import GotToMenu from './buttons/GotToMenu.vue';
 
+
 export default {
+  data() {
+    return {
+      map: null,
+      tools: null
+    }
+  },
   mounted() {
     // Inicjalizacja mapy OpenLayers
-    const map = initializeMap('map');
-    this.$data.map = map;
+    // const map = initializeMap('map');
+    const { map, tools } = initializeMap('map');
+    this.map = map;
+    this.tools = tools
   },
   beforeUnmount() {
     // Zniszczenie mapy przed usunięciem komponentu
-    if (this.$data.map) {
-      this.$data.map.setTarget(null);
-      this.$data.map = null;
+    if (this.map) {
+      this.map.setTarget(null);
+      this.map = null;
+    }
+  },
+  methods: {
+    centerMap() {
+      console.log("CENTER", this.tools )
+      this.tools.centerMap()
     }
   },
   components: {
@@ -29,8 +46,24 @@ export default {
 </script>
 
 <style scoped>
-.map-container {
-  width: 100%;
-  height: 100vh; /* Zmieniona wysokość na 100% widoku */
-}
+  .map-container {
+    width: 100%;
+    height: 100vh; /* Zmieniona wysokość na 100% widoku */
+  }
+  #autoUpdateButton {
+    position: absolute;
+    top: 100px;
+    left: 9px;
+    z-index: 1000;
+    border: none;
+    border-radius: 2px;
+  }
+  .ol-zoom {
+    right: 10px;
+  }
+
+  .ol-control {
+    top: initial;
+    bottom: 10px;
+  }
 </style>
--- displayapp/src/logic/maps.js
+++ displayapp/src/logic/maps.js
@@ -3,44 +3,96 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
-import { fromLonLat } from 'ol/proj';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import { Style, Icon } from 'ol/style';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
+import { fromLonLat } from 'ol/proj';
 
 export function initializeMap(targetElement) {
-  const map = new Map({
-    target: targetElement,
-    layers: [
-      new TileLayer({
-        source: new XYZ({
-          url: 'http://localhost:9001/get-map/{z}/{x}/{y}',
+
+    let autoPositionUpdate = true;
+    let coordinates = fromLonLat([21.0122, 52.2297]);
+
+    const tools = {
+        centerMap: function() {
+            autoPositionUpdate = true;
+            map.getView().setCenter(coordinates);
+        },
+    };
+    
+    const map = new Map({
+        target: targetElement,
+        layers: [
+            new TileLayer({
+                source: new XYZ({
+                url: 'http://localhost:9001/get-map/{z}/{x}/{y}',
+                }),
+            }),
+        ],
+        view: new View({
+            center: fromLonLat([21.0122, 52.2297]),
+            zoom: 12,
         }),
-      }),
-    ],
-    view: new View({
-      center: fromLonLat([21.0122, 52.2297]),
-      zoom: 12,
-    }),
-  });
+    });
 
-  // Dodanie warstwy wektorowej z markerem
-  const vectorLayer = new VectorLayer({
-    source: new VectorSource({
-      features: [new Feature(new Point(fromLonLat([21.0122, 52.2297])))],
-    }),
-    style: new Style({
+    // Ustaw ścieżkę do własnej ikony
+    const iconPath = 'target.png';
+
+    const markerStyle = new Style({
       image: new Icon({
-        anchor: [0.5, 46],
+        anchor: [0.5, 1], // Ustaw punkt zakotwiczenia na środek dolnej krawędzi ikony
         anchorXUnits: 'fraction',
-        anchorYUnits: 'pixels',
-        src: 'https://openlayers.org/en/latest/examples/data/icon.png', // Ścieżka do ikony markera
+        anchorYUnits: 'fraction',
+        src: iconPath,
       }),
-    }),
-  });
-  map.addLayer(vectorLayer);
+    });
 
-  return map;
+    const markerLayer = new VectorLayer({
+        source: new VectorSource(),
+    });
+
+    map.addLayer(markerLayer);
+
+    // Utwórz WebSocket
+    const socket = new WebSocket('ws://localhost:50250');
+
+    socket.onmessage = function (event) {
+        const data = JSON.parse(event.data);
+
+        // Aktualizuj pozycję na mapie
+        coordinates = fromLonLat([parseFloat(data.longitude), parseFloat(data.latitude)]);
+        const marker = new Feature(new Point(coordinates));
+        marker.setStyle(markerStyle);
+
+        // Usuń poprzednią warstwę z markerem
+        markerLayer.getSource().clear();
+
+        // Dodaj nową warstwę z aktualnym markerem
+        if (marker)
+        markerLayer.getSource().addFeature(marker);
+
+        if(autoPositionUpdate) {
+            // Ustaw nowy środek widoku mapy na aktualną lokalizację
+            map.getView().setCenter(coordinates);
+        }
+    };
+
+    map.on('pointerdown', function () {
+        console.log('pointerdown');
+        autoPositionUpdate = false
+    });
+
+    // Dodaj obsługę zamknięcia WebSocket
+    socket.onclose = function (event) {
+        console.error('Połączenie WebSocket zostało zamknięte:', event);
+    };
+
+    // Dodaj obsługę błędu WebSocket
+    socket.onerror = function (error) {
+        console.error('Błąd WebSocket:', error);
+    };
+
+    return { map, tools };
 }
